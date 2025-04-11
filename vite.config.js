30812c6c3b57d2ecfe889bbb61ba761a55c10e18
supilog <supi.gm+supilog@gmail.com>
2025-04-11 14:12:21 +0900

    vire

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         laravel({
             input: [
                 'resources/scss/app.scss',
